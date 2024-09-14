b2a9285d3008233d9f6d1fcf7a49b782fc91ebd2
jayanta1483 <jsarkar14683@gmail.com>
2024-09-14 14:52:02 +0530

    added lodash throttle

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import {
   imageSlidePrev,
   slideOnClick,
 } from "./js/eventHandlers.js";
+import _ from "lodash";
 
 export { carouselSetup, initiateAutoSlide };
 
@@ -14,9 +15,9 @@ const host = document.querySelector("#carouselHost");
 const prevBtn = host.shadowRoot.querySelector("#prev");
 const nextBtn = host.shadowRoot.querySelector("#next");
 
-nextBtn.addEventListener("click", imageSlideNext);
+nextBtn.addEventListener("click", _.throttle(imageSlideNext, 900));
 
-prevBtn.addEventListener("click", imageSlidePrev);
+prevBtn.addEventListener("click", _.throttle(imageSlidePrev, 900));
 
 const indicatorContainer = host.shadowRoot.querySelector(
   ".carousel__indicators"
